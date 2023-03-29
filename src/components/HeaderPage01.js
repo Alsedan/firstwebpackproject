3293c582f027417ccb877bb218a456046541a621
Alsedan <sedanalan@gmail.com>
2023-03-29 00:18:54 -0300

    First Commit

---
+++ src/components/HeaderPage01.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Menu(props) {
+    return(
+        <p>{props.name}</p>
+    );
+}
+
+export function HeaderPage01(){
+    return(
+        <h1>Headerpage01</h1>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom"
+import Page01 from "./Page01";
+
+
+const container = document.getElementById("root");
+container ? ReactDOM.render(<Page01 />, container) : false;
