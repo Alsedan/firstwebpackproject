d70c2648c3c75220247a92f7af1dfae52a816b17
Alsedan <sedanalan@gmail.com>
2023-04-12 19:55:38 -0300

    backup

--- src/components/HeaderPg1.js
+++ src/components/HeaderPg1.js
@@ -1,9 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import fundobloco01 from "../assets/fundobloco01.jpg"
 
+const Imagebloco01 = styled.img`
+width: 100%;
+height: 700px;
+position: relative;
+margin: none;
+background-size: cover;
+background-image: url(${fundobloco01});
+`
 const Headerconst = styled.div `
 background-color:rgba(45,102,171,0.9);
 display:flex;
+position:absolute;
 width:100%;
 height:60px;
 align-items: center;
@@ -28,6 +38,7 @@ margin-right: 50px;
 export function Headerstyle() {
     return(
          <Headerconst>
+            <Imagebloco01></Imagebloco01>
             <Headernavbaritens>Produtos</Headernavbaritens>
             <Headernavbaritens>Suporte</Headernavbaritens>
             <Headernavbaritens>Sobre a kaleb</Headernavbaritens>
--- src/pages/Page01.js
+++ src/pages/Page01.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Headerstyle } from "../components/HeaderPg1";
 import Global from "../theme/GlobalStyle";
-import { Bloco01 } from "../components/bloco01";
 
 export default function Page01() {
         return (
           <React.Fragment>
-            <Bloco01/>
-            <React.Fragment>
             <Headerstyle/>
             <Global/>
-            </React.Fragment>
           </React.Fragment>
            
     );
